905f5765244c4f6e85170b0d2b9f1f7e63ecf4e2
CagriAyisik <cagriayisik@gmail.com>
2026-08-28 02:25:32 +0300

    Fix PWABuilder icons and manifest 404 errors

--- sw.js
+++ sw.js
@@ -1,21 +1,38 @@
-const CACHE_NAME = 'megane-tablet-v1';
+const CACHE_NAME = 'megane-tablet-v2';
 const ASSETS = [
   './',
   './index.html',
   './css/style.css',
   './js/sound.js',
+  './js/clock.js',
   './js/gauge.js',
   './js/weather.js',
   './js/news.js',
   './js/media.js',
   './js/app.js',
-  './manifest.json'
+  './manifest.json',
+  './assets/icon-192.png',
+  './assets/icon-512.png',
+  './assets/icon-maskable.png',
+  './assets/favicon.png'
 ];
 
 self.addEventListener('install', (e) => {
   e.waitUntil(
     caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
   );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+  self.clients.claim();
 });
 
 self.addEventListener('fetch', (e) => {
